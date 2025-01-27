0a5073bf7e8ec3041a0aa6bb820a9a11e8dbb664
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-27 02:39:33 -0500

    WIP

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { ethers } from 'ethers';
 import { weth9ABI } from './weth9ABI.js';
 
 const ETHEREUM = 1;
 const POLYGON = 137;
 const HARDHAT = 31337;
 const BURN_ADDRESS = "0x0000000000000000000000000000000000000000";
+const ethers = hre.ethers;
 
 class WethMethods {
 
